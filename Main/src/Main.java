63a67d1187ed6e691d6950c0a06d2bd2b19e3412
Nguyen Huy Vu <Nguyen Huy Vu@LAPTOP-F8E55ES0>
2024-04-02 22:28:32 +0700

    Initial Commit.

---
+++ Main/src/Main.java
@@ -0,0 +1,6 @@
+public static void main(String[] args) {
+        int a = 5, b = 3;
+        swap(a, b);
+        System.out.println("a sau khi swap: " + a);
+        System.out.println("b sau khi swap: " + b);
+    }
