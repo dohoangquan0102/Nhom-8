0789450db12e0a7989bfef28ba00a3f7c445e037
dohoangquan0102 <161783919+dohoangquan0102@users.noreply.github.com>
2024-04-02 22:23:37 +0700

    Initial Commit.

---
+++ manhcuong/src/manhcuong/print.java
@@ -0,0 +1,13 @@
+package manhcuong;
+
+public class print {
+	public static void swap(int a, int b) {
+        int c = a;
+        a = b;
+        b = c;
+        System.out.println("Ket qua swap: " + "a = " + a + " b= " + b);
+    }
+    public static void main(String[] args) {
+        int a , b;
+    }
+}
